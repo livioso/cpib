02ee0df35147de675b727576c53b795df156cb8f
Livio Bieri <livio@livio.li>
2015-10-04 22:25:47 +0200

    Terminals added

---
+++ Compiler/Compiler/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Compiler
+//
+//  Created by Livio Bieri on 30/09/15.
+//  Copyright (c) 2015 Livio Bieri. All rights reserved.
+//
+
+import Foundation
+
+print("IML Compiler 👻")
+
+var terminal = Terminal.COMMENT
+print(terminal)
+print(terminal.rawValue)
+
