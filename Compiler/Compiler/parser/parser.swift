f8d3071ba54ab285c608aa6f9827b8e4ce415766
Livio Bieri <livio@livio.li>
2015-11-18 19:27:25 +0100

    Scanner initial parse method added

--- Compiler/Compiler/parser/parser.swift
+++ Compiler/Compiler/parser/parser.swift
@@ -1,9 +1,7 @@
-//
-//  parser.swift
-//  Compiler
-//
-//  Created by Livio Bieri on 18/11/15.
-//  Copyright © 2015 Livio Bieri. All rights reserved.
-//
-
 import Foundation
+
+class Parser {
+	func parse (let tokenlist: [Token]) {
+		
+	}
+}
